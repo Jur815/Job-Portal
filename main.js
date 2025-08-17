c29d032041d565abbcf942165bd9f5bc638abd48
Peter Jur <pmakec@gmail.com>
2025-08-17 20:05:55 +0200

    Adding more code

---
+++ main.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menu-btn");
+const navlinks = document.getElementById("nav-links");
+const menuBtnIcon = document.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navlinks.classList.toggle("open");
+
+  const isOpen = navlinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
